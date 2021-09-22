fff312c7dd1a7d631c68a3d9d6e6625d0a6b28b0
Karine Rebelatto <karinerebelatto@gmail.com>
2021-09-22 00:10:24 -0300

    semana6-aula21

---
+++ semana6/LabenuForms/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react"
+import Form1 from "./pages/Form1";
+import Form2 from "./pages/Form2";
+import Form3 from "./pages/Form3";
+import Final from "./pages/Final";
+
+function App() {
+  return (
+    <div>
+      div vazia
+    </div>
+  );
+}
+
+export default App;
